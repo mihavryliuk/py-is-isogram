9dc3c1b5baaa695a2f98cd0b32e6ef1b7fa1531c
Misha Havryliuk <misharob31415@gmail.com>
2024-08-07 16:49:08 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_empty_string() -> None:
+    assert is_isogram("") is True
+
+
+def test_upper_letter() -> None:
+    assert is_isogram("Adam") is False
+
+
+def test_repeat_letter() -> None:
+    assert is_isogram("look") is False
+
+
+def test_no_repeat_letter() -> None:
+    assert is_isogram("playgrounds") is True
